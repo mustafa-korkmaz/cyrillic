b41c9a66ddded7ddcf46637c519fd7ab0d33c4b5
Mustafa Korkmaz (V) <Mustafa.Korkmaz@shutterfly.com>
2019-10-09 11:46:26 +0300

    ignore modified

--- src/app/firebase.crendentials.ts
+++ src/app/firebase.crendentials.ts
@@ -1,10 +1,10 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyBPuvMMwC81upBn70TL26KKsHmfZ3nN62Y",
+    apiKey: 'AIzaSyBPuvMMwC81upBn70TL26KKsHmfZ3nN62Y',
     authDomain: "cyrillic-e697c.firebaseapp.com",
-    databaseURL: "https://cyrillic-e697c.firebaseio.com",
-    projectId: "cyrillic-e697c",
-    storageBucket: "",
-    messagingSenderId: "43770263242",
-    appId: "1:43770263242:web:01915f8320ea0d0411e633",
-    measurementId: "G-B1YYQXH21N"
+    databaseURL: 'https://cyrillic-e697c.firebaseio.com',
+    projectId: 'cyrillic-e697c',
+    storageBucket: '',
+    messagingSenderId: '43770263242',
+    appId: '1:43770263242:web:01915f8320ea0d0411e633',
+    measurementId: 'G-B1YYQXH21N'
   };
