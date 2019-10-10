78c2ca6e117a76e3e78dd303c3e073a9b0259b8f
Mustafa Korkmaz <mustafa.korkmaz@scalefocus.com>
2019-10-10 17:31:43 +0300

    bulgarian alphabet references added

--- src/app/alphabet/alphabet.model.ts
+++ src/app/alphabet/alphabet.model.ts
@@ -2,4 +2,10 @@ export class Alphabet {
     key: string;
     type: number;
     char: string;
+    ref: LanRef;
+}
+
+export interface LanRef {
+    type: number;
+    value: string;
 }
--- src/app/alphabet/alphabet.service.ts
+++ src/app/alphabet/alphabet.service.ts
@@ -23,11 +23,11 @@ export class AlphabetService {
     const en = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    const tr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    const tr = ['A', 'B', 'C', 'Ç', 'D', 'E', 'F', 'G', 'H', 'I', 'İ', 'J', 'K', 'L', 'M',
       'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-    const bu = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    const bu = [['A', 'A', 'A'], ['Б'], ['B'], ['C'], ['Ч'], ['E'], ['Ф'], ['Г'], ['H'], ['Ъ'], ['Ж'], ['K'], ['Л'], ['M'],
+    ['И'], ['O'], ['P'], ['П'], ['Ш'], ['Щ'], ['T'], ['Ц'], ['Ю'], ['Y'], ['Я'], ['X'], ['З'], ['Й']];
 
     const allAlphabets: Alphabet[] = [];
 
@@ -36,7 +36,7 @@ export class AlphabetService {
       a.char = en[i] + en[i].toLowerCase();
       a.type = 0;
       a.key = en[i];
-
+      a.ref = null;
       allAlphabets.push(a);
     }
 
@@ -45,17 +45,17 @@ export class AlphabetService {
       a.char = tr[i] + tr[i].toLowerCase();
       a.type = 1;
       a.key = tr[i];
-
+      a.ref = null;
       allAlphabets.push(a);
     }
 
-    for (let i = 0; i <= bu.length - 1; i++) {
-      const a = new Alphabet();
-      a.char = bu[i] + bu[i].toLowerCase();
-      a.type = 2;
-      a.key = bu[i];
+    // for (let i = 0; i <= bu.length - 1; i++) {
+    //   const a = new Alphabet();
+    //   a.char = bu[i] + bu[i].toLowerCase();
+    //   a.type = 2;
+    //   a.key = bu[i];
 
-      allAlphabets.push(a);
-    }
+    //   allAlphabets.push(a);
+    // }
   }
 }
